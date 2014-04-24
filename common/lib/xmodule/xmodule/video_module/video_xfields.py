20b6584e2bfa0a47a2debe8aad69647702ec0de8
polesye <s2pak.anton@gmail.com>
2014-04-24 12:30:38 +0300

    Fix acceptance test.

--- common/lib/xmodule/xmodule/video_module/video_xfields.py
+++ common/lib/xmodule/xmodule/video_module/video_xfields.py
@@ -160,7 +160,7 @@ class VideoFields(object):
 
     scored_on_end = Boolean(
         help="Grades the video on video end.",
-        display_name="Scored on video end.",
+        display_name="Scored on video end",
         scope=Scope.settings,
         default=False,
     )
