b7d3216e833da6554d68912827031bb3d87413f7
Valera Rozuvan <valera@edx.org>
2014-04-30 09:54:48 +0000

    Work in progress.

--- common/lib/xmodule/xmodule/js/src/video/10_graders.js
+++ common/lib/xmodule/xmodule/js/src/video/10_graders.js
@@ -5,41 +5,43 @@ define(
 ['video/00_abstract_grader.js'],
 function (AbstractGrader) {
     /**
-     * GraderChooser module.
+     * GraderCollection module.
      * @exports video/10_graders.js
      * @constructor
      * @param {object} state The object containing the state of the video
      * player.
      * @return {jquery Promise}
      */
-    var GraderChooser = function (state, i18n) {
-        if (!(this instanceof GraderChooser)) {
-            return new GraderChooser(state, i18n);
+    var GraderCollection = function (state, i18n) {
+        var _this;
+
+        if (!(this instanceof GraderCollection)) {
+            return new GraderCollection(state, i18n);
         }
 
         debugger;
 
-        var graderName = state.config.graderName,
-            _this = this;
-
         this.graderInstances = [];
+        this.graderMap = {
+            'scored_on_end': 'GradeOnEnd',
+            'scored_on_percent': 'GradeOnPercent'
+        };
 
         if (state.config.hasScore) {
-            $.each(state.config.graders, function (graderName, graderConfig) {
-                var graderInstance;
+            _this = this;
+
+            $.each(state.config.graders, function (backendPropertyName, graderConfig) {
+                var graderName = _this.graderMap[backendPropertyName],
+                    graderInstance;
 
                 debugger;
 
-                if (GraderChooser[graderName]) {
-                    graderInstance = new GraderChooser[graderName](state, i18n, graderConfig);
+                if (GraderCollection[graderName]) {
+                    graderInstance = new GraderCollection[graderName](state, i18n, graderConfig);
 
                     _this.graderInstances.push(graderInstance);
                 }
             });
-
-            // if (GraderChooser[graderName]) {
-            //     return new GraderChooser[graderName](state, i18n);
-            // }
         }
 
         return $.Deferred().resolve().promise();
@@ -47,16 +49,31 @@ function (AbstractGrader) {
 
     // Write graders below this line
 
-    GraderChooser.GradeOnEnd = AbstractGrader.extend({
+    GraderCollection.GradeOnEnd = AbstractGrader.extend({
         getGrader: function (element, state) {
             var dfd = $.Deferred();
             element.on('ended', dfd.resolve);
 
             return dfd.promise();
-        }
+        },
+
+        backendPropertyName: 'scored_on_end'
     });
 
-    return GraderChooser;
+    GraderCollection.GradeOnPercent = AbstractGrader.extend({
+        getGrader: function (element, state) {
+            var dfd = $.Deferred();
+
+            // TODO: Implement success scenario.
+            // element.on('ended', dfd.resolve);
+
+            return dfd.promise();
+        },
+
+        backendPropertyName: 'scored_on_percent'
+    });
+
+    return GraderCollection;
 });
 
 }(window.RequireJS.define));
